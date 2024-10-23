f007728f035a5665eeb3e9d5cda71854d318c0a5
Moïse Nturubika <nturumoussa@gmail.com>
2024-10-23 19:30:45 +0100

    [FIX] Update category urls when update data

--- tax_car/car/urls.py
+++ tax_car/car/urls.py
@@ -3,5 +3,5 @@ from car.views import CarCategoryApiView, CarCategoryDetailApiView
 
 urlpatterns = [
     path('category', CarCategoryApiView.as_view()),
-    path('category/<int:category_id>/', CarCategoryDetailApiView.as_view())
+    path('category/<int:category_id>', CarCategoryDetailApiView.as_view())
 ]
