1c3c8b7eaadb80fb095f173726c8c1b5ce0fb697
Moïse Nturubika <nturumoussa@gmail.com>
2024-10-31 12:20:09 +0100

    [ENH] Make quittance_number nullable 1

--- tax_car/payment/models.py
+++ tax_car/payment/models.py
@@ -6,15 +6,15 @@ import uuid
 class PaymentTax(models.Model):
     date_paiement = models.DateTimeField(db_column="date_paiement", auto_now_add=True)
     montant = models.FloatField(db_column="montant")
+    # quittance_number = models.CharField(max_length=10, editable=False, unique=True, null=True, db_column="quittance_number")
     ref_car = models.ForeignKey(Car, on_delete=models.DO_NOTHING, db_column="ref_car")
     ref_perceptor = models.ForeignKey(Perceptor, on_delete=models.DO_NOTHING, db_column="ref_perceptor")
-    quittance_number = models.CharField(max_length=10, editable=False, unique=True, null=True, db_column="quittance_number")
 
     class Meta:
         db_table = 'tb_payment_tax'
 
-    def save(self, *args, **kwargs):
-        if not self.quittance_number:
-            unique_id = uuid.uuid4().int  # Generate a UUID-based integer
-            self.quittance_number = f"{unique_id % 10**8:06d}"  # Get last 6 digits of UUID and format
-        super(PaymentTax, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if not self.quittance_number:
+    #         unique_id = uuid.uuid4().int  # Generate a UUID-based integer
+    #         self.quittance_number = f"{unique_id % 10**8:06d}"  # Get last 6 digits of UUID and format
+    #     super(PaymentTax, self).save(*args, **kwargs)
