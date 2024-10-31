2316a3ee643ccc43615e8f4d1cc71c9e59eeb07d
Moïse Nturubika <nturumoussa@gmail.com>
2024-10-31 10:04:33 +0100

    [FIX] Add cors-headers settings

--- tax_car/tax_car/settings.py
+++ tax_car/tax_car/settings.py
@@ -43,10 +43,12 @@ INSTALLED_APPS = [
     'car',
     'payment',
     'perceptor',
-    'djoser'
+    'djoser',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
